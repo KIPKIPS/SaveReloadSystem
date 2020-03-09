4c4b846d2663a3af01aa985f2b8b8b1bd3be2dfe
RECNEPS-KIPS <2432719656@qq.com>
2020-03-09 16:52:23 +0800

    挂起学习计划

---
+++ Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour {
+    public GameObject[] monster;//怪物数组
+    public GameObject activeMonster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActiveMonster();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ActiveMonster() {
+        int randomIndex = Random.Range(0, monster.Length);
+        activeMonster = monster[randomIndex];
+        activeMonster.SetActive(true);
+        activeMonster.GetComponent<BoxCollider>().enabled=true;
+    }
+}
