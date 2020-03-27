8bcf2e7f253f28a26047752280036bf385d7e142
RECNEPS-KIPS <2432719656@qq.com>
2020-03-27 20:43:59 +0800

    实现怪物的受击死亡

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour {
+    private Vector3 firePos;
+    private Vector3 tailPos;
+    private Vector3 fireDir;//开火方向
     //竖直
     private float maxXRotationt = 80;
     private float minXRotationt = 0;
@@ -11,28 +14,40 @@ public class GunController : MonoBehaviour {
     private float minYRotationt = 0;
 
     //射击间隔
-    private float shootTime = 1;
+    private float shootTime = 0.5f;
     private float timer = 0;//计时器
+
+    private GameObject bullet;
     // Start is called before the first frame update
     void Start() {
-
+        bullet = Resources.Load<GameObject>("Bullet");
     }
 
     // Update is called once per frame
     void Update() {
+        firePos = GameObject.Find("FirePos").transform.position;
+        tailPos = GameObject.Find("TailPos").transform.position;
+        //xy位置与屏幕宽高的百分比
+        float xPos = Input.mousePosition.x / Screen.width;
+        float yPos = Input.mousePosition.y / Screen.height;
+        float xAngle = -Mathf.Clamp(yPos * maxXRotationt, minXRotationt, maxXRotationt) + 20;//竖直方向的旋转角
+        float yAngle = Mathf.Clamp(xPos * maxYRotationt, minYRotationt, maxYRotationt) - 60;//水平方向的旋转角
+
+        //实现旋转
+        transform.eulerAngles = new Vector3(xAngle, yAngle, 0);
+
         timer += Time.deltaTime;
         if (timer>=shootTime) {
             //TODO:可以射击
-
+            if (Input.GetMouseButtonDown(0)) {
+                GameObject go = Instantiate(bullet, firePos, Quaternion.identity);
+                fireDir = firePos - tailPos;
+                Debug.DrawLine(tailPos,firePos,Color.blue,5);
+                go.GetComponent<Rigidbody>().AddForce(fireDir.normalized*2000);
+                timer = 0;
+            }
         }
-        //xy位置与屏幕宽高的百分比
-        float xPos = Input.mousePosition.x/Screen.width;
-        float yPos= Input.mousePosition.y / Screen.height;
-        float xAngle = -Mathf.Clamp(yPos * maxXRotationt, minXRotationt, maxXRotationt)+20;//竖直方向的旋转角
-        float yAngle = Mathf.Clamp(xPos * maxYRotationt, minYRotationt, maxYRotationt)-60;//水平方向的旋转角
-
-        //实现旋转
-        transform.eulerAngles=new Vector3(xAngle,yAngle,0);
+        
 
     }
 }
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+    private Animation anim;
+
+    private bool die;
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag=="Bullet") {
+            StartCoroutine(AnimateChange());
+        }
+    }
+    IEnumerator AnimateChange() {
+        if (die == false) {
+            anim.Play(name + "_die");
+            die = true;
+        }
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+}
