a1e0953df10bfb1ac80f91bc01295027f97aa912
RECNEPS-KIPS <2432719656@qq.com>
2020-03-27 16:13:18 +0800

    实现怪物的销毁

--- Assets/Scripts/MonsterManager.cs
+++ Assets/Scripts/MonsterManager.cs
@@ -6,28 +6,41 @@ public class MonsterManager : MonoBehaviour {
     public GameObject[] monster;//怪物数组
     public GameObject activeMonster;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         //ActiveMonster();
         StartCoroutine(AliveTimer());
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     void ActiveMonster() {
         int randomIndex = Random.Range(0, monster.Length);
         activeMonster = monster[randomIndex];
         activeMonster.SetActive(true);
-        activeMonster.GetComponent<BoxCollider>().enabled=true;
+        activeMonster.GetComponent<BoxCollider>().enabled = true;
+        StartCoroutine(DeathTimer());
     }
-
+    //随机激活怪物时间
     IEnumerator AliveTimer() {
         //yield return 12;//12帧之后执行
-        yield return new WaitForSeconds(Random.Range(1,5));
+        yield return new WaitForSeconds(Random.Range(1, 4));
         ActiveMonster();
+    }
+    //将激活的怪物取消激活
+    public void Deactive() {
+        if (activeMonster!=null) {
+            activeMonster.GetComponent<BoxCollider>().enabled = false;
+            activeMonster.SetActive(false);
+            activeMonster = null;
+        }
+        StartCoroutine(AliveTimer());
+    }
+    //死亡时间
+    IEnumerator DeathTimer() {
+        yield return new WaitForSeconds(Random.Range(3,8));
+        Deactive();
     }
 }
