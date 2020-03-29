79db72b2a93a0de913022b8d4b15e01066141b3e
RECNEPS-KIPS <2432719656@qq.com>
2020-03-29 14:05:53 +0800

    暂停的TimeScale有问题

---
+++ Assets/Scripts/ShowPauseBtn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPauseBtn : MonoBehaviour {
+    public GameObject pauseBtn;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void ShowBtn() {
+        pauseBtn.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void Pause() {
+        Time.timeScale = 0;
+    }
+}
